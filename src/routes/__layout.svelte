d3d7705bfba7e6007e119c26b590909f5b007765
snl95 <snl95@txstate.edu>
2022-04-29 10:03:00 -0500

    nav and layouts

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,14 @@
 </script>
 
 <header>
-	<Title title="Svelte Gaming" />
+	<nav>
+		<Title title="Ninja Gaming Guides" />
+		<div class="links">
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+			<a href="/guides">Guides</a>
+		</div>
+	</nav>
 </header>
 <main>
 	<slot />
@@ -15,8 +22,27 @@
 
 <style>
 	header {
+		text-align: center;
+		background: rgba(0, 0, 0, 0.1);
+	}
+	main {
+		max-width: 960px;
+		margin: 20px auto;
+	}
+	footer {
+		text-align: center;
+	}
+	nav {
 		display: flex;
-		justify-content: center;
+		align-items: center;
+		max-width: 960px;
+		margin: 0 auto;
+	}
+	.links {
+		margin-left: auto;
+	}
+	a {
+		margin-left: 10px;
 	}
 	main {
 		max-width: 960px;
--- src/routes/guides/index.svelte
+++ src/routes/guides/index.svelte
@@ -4,6 +4,20 @@
 		<li><a href="/guides/dark-souls-ii">Dark Souls II</a></li>
 		<li><a href="/guides/dark-souls-iii">Dark Souls III</a></li>
 	</ul>
-	<a href="/about">About</a>
-	<a href="/">Home</a>
 </div>
+
+<style>
+	.guides {
+		margin-top: 20px;
+	}
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	a {
+		display: inline-block;
+		margin-top: 10px;
+		padding: 10px;
+		border: 1px dotted rgba(255, 255, 255, 0.2);
+	}
+</style>
